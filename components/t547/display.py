8e61276a9bb6a2ae3118fbd1120d15f93cf56f8d
Louis Matthijssen <louis@u5r.nl>
2025-09-25 19:14:16 +0200

    Dont use variables

--- components/t547/display.py
+++ components/t547/display.py
@@ -50,4 +50,4 @@ async def to_code(config):
     cg.add_build_flag("-DBOARD_HAS_PSRAM")
 
     cg.add_library("LilyGoEPD47", repository="https://github.com/Xinyuan-LilyGO/LilyGo-EPD47", version="v1.0.1")
-    cg.add_build_flag("-I$PROJECT_DIR/.esphome/build/$NAME/.piolibdeps/$NAME/LilyGoEPD47/src")
+    cg.add_build_flag("-I/config/.esphome/build/voordeur/.piolibdeps/voordeur/LilyGoEPD47/src")
